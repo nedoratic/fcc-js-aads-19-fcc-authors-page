a7876dc84debfc0a43e25a70762081ec46c25bed
nedoratic <nedoratic@gmail.com>
2024-06-01 21:49:09 +0200

    index.js - chaining then method to fetch method and loging the data to the console

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ const authorContainer = document.getElementById('author-container');
 
 const loadMoreBtn = document.getElementById('load-more-btn');
 
+// The fetch() method returns a Promise, which is a placeholder object that will either be fulfilled if your request is successful, or rejected if your request is unsuccessful. If the Promise is fulfilled, it resolves to a Response object, and you can use the .then() method to access the Response.
+
 // Fetch authors data
-fetch('https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json');
+fetch('https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json').then((res) => console.log(res));
