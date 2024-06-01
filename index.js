3536de54b0275d6ca8439149c7a880802190f593
nedoratic <nedoratic@gmail.com>
2024-06-01 21:43:41 +0200

    index.js - fetch authors data

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const authorContainer = document.getElementById('author-container');
 
 const loadMoreBtn = document.getElementById('load-more-btn');
+
+// Fetch authors data
+fetch('https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json');
